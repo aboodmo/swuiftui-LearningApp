9f762cb2d90388b273884378430cefb783291cae
abood <abdelrahman.mohammad02@gmail.com>
2022-06-02 19:33:39 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Abdelrahman Mohammad on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
